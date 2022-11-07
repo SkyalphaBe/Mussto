83d9f4da34948656374dff6bb2e493734a36c2b8
François Graux <93640820+SkyalphaBe@users.noreply.github.com>
2022-11-07 21:06:56 +0100

    fix de l'accueil + avancement sidebarre etudiant

--- code/views/component/sideBarre.php
+++ code/views/component/sideBarre.php
@@ -1,18 +1,22 @@
 <div class="sideBarre">
     <div class="topBox">
-        <div class="titre">
-            <h1>musst</h1>
-            <h1 id="oTitre">o</h1>
+        <div class="titleBox">
+            <div class="titre">
+                <h1>musst</h1>
+                <h1 id="oTitre">o</h1>
+            </div>
+            <h2>Etudiant</h2>
         </div>
-        <h3>Etudiant</h3>
-    </div>
-    <div class="menu">
-        <h4>Menu</h4>
-        <a href="">Acceuil</a>
-        <a href="">Mes modules</a>
+        <div class="ligneSep"></div>
+        <div class="menu">
+            <h3>Menu</h3>
+            <a href="">Acceuil</a>
+            <a href="">Mes modules</a>
+        </div>
     </div>
     <div class="botBox">
-        <h4>Profil</h4>
+        <div class="ligneSep"></div>
+        <h3>Profil</h3>
         <div class="account">
             <div class="box">
                 <div class="circle"></div>
@@ -22,6 +26,8 @@
                 <p>Last Name</p>
             </div>
         </div>
-        <button>Deconnexion</button>
+        <div class="button">
+            <button class="decoButton">Deconnexion</button>
+        </div>
     </div>
 </div>
--- code/views/v_AcceuilEtu.php
+++ code/views/v_AcceuilEtu.php
@@ -5,31 +5,33 @@
     <link rel="stylesheet" href="<?php echo PATH_CSS ?>sideBarre.css">
     <link rel="stylesheet" href="<?php echo PATH_CSS ?>accueilEtu.css">
     <link rel="stylesheet" href="<?php echo PATH_CSS ?>accueilEtuGrid.css">
-    <title>Page acceuil</title>
+    <title>Page accueil</title>
 </head>
 <body>
     <?php
         require_once(PATH_VIEW_COMPONENT.'sideBarre.php');
     ?>
-    <div id="content">
-        <div id="welcome">
+    <div class="content">
+        <div class="welcome">
             <img id="student" src="assets/images/etudiant.png">
-            <span id="msg">
+            <div class="msg">
                 <h2>Bonjour <?= $_SESSION["name"]?> !</h2>
                 <p>Tu as des nouvelles fraîches</p>
-            </span>
+            </div>
         </div>
-        <div id="lastMark" class="info">
-            <h2>Dernière note</h2>
-        </div>
-        <div id="sondage" class="info">
-            <h2>Sondages</h2>
-        </div>
-        <div id="othersMarks" class="info">
-            <h2>Autres notes</h2>
-        </div>
-        <div id="DS" class="info">
-            <h2>DS à venir</h2>
+        <div class="informationEleve">
+            <div id="lastMark" class="info">
+                <h2>Dernière note</h2>
+            </div>
+            <div id="sondage" class="info">
+                <h2>Sondages</h2>
+            </div>
+            <div id="othersMarks" class="info">
+                <h2>Autres notes</h2>
+            </div>
+            <div id="DS" class="info">
+                <h2>DS à venir</h2>
+            </div>
         </div>
     </div>
 </body>
