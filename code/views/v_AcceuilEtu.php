8aca3306fd5eafda93922903d4e3644ef18d7927
Adrien BEGASSAT <p2100104@edu.univ-lyon1.fr>
2022-11-10 11:10:36 +0100

    ajout couleur devoir

--- code/views/v_AcceuilEtu.php
+++ code/views/v_AcceuilEtu.php
@@ -26,7 +26,7 @@ require_once(PATH_VIEW_COMPONENT.'header.php');
         <h2>DS à venir</h2>
         <?php if (isset($devoir_coming) && $devoir_coming){
             foreach($devoir_coming as $devoir) { ?>
-                <div style="background-color: hsl(<?=random_int(0, 360)?>, 66%, 41%)" class="devoir">
+                <div style="background-color: hsl(<?=hexdec(substr(bin2hex($devoir['NOMMODULE']), 0, 16))%360?>, 66%, 41%)" class="devoir">
                     <h3 class="devoir-module-name"><?=$devoir['NOMMODULE']?></h3>
                     <p class="devoir-date"><?=$devoir['DATEDEVOIR']?></p>
                     <p class="devoir-salle">SALLE <?=$devoir['SALLE']?></p>
