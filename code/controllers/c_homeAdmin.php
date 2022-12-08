d1651f262b5cd9da44f8937ee3371b46d9ef08d5
François Graux <93640820+SkyalphaBe@users.noreply.github.com>
2022-12-08 12:51:24 +0100

    création méthode crée compte etu est prof

--- code/controllers/c_homeAdmin.php
+++ code/controllers/c_homeAdmin.php
@@ -1,3 +1,9 @@
 <?php
+    require_once(PATH_MODELS.'AdminDAO.php');
+
+    $dao = new AdminDAO(true, $_SESSION['login']);
+
+    /*$testP = $dao->createProf("bensligoat","1234","djamal","benslimane","PROFESSEUR");
+    $testE =$dao->createProf("Basti","1234","Bastien","Puscedo","ETUDIANT");*/
     require_once(PATH_VIEWS.'AccueilAdmin.php');
 ?>
--- code/models/AdminDAO.php
+++ code/models/AdminDAO.php
@@ -2,6 +2,14 @@
 require_once (PATH_MODELS.'UserDAO.php');
 class AdminDAO extends UserDAO
 {
-
+    public function createProf($login,$password,$prenom,$nom,$typeCompte){
+        $mdp = password_hash($password, PASSWORD_DEFAULT);
+        if($typeCompte == "PROFESSEUR"){
+            $this->insertRow("insert into PROFESSEUR values (?,?,?,?)",[$login,$mdp,$prenom,$nom]);
+        }
+        else if($typeCompte == "ETUDIANT"){
+            $this->insertRow("insert into ETUDIANT values (?,?,?,?)",[$login,$mdp,$prenom,$nom]);
+        }
+    }
 }
 ?>
