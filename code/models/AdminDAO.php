be707c038eea89ef0fd1d89fd07a46cecc0a22f6
Louis LACHEREZ <louis.lacherez@etu.univ-lyon1.fr>
2022-12-09 13:22:11 +0100

    AdminDAO getters

--- code/models/AdminDAO.php
+++ code/models/AdminDAO.php
@@ -2,6 +2,24 @@
 require_once (PATH_MODELS.'UserDAO.php');
 class AdminDAO extends UserDAO
 {
+    public function getAllModules(){
+        $res = $this->queryAll("SELECT REFMODULE, NOMMODULE FROM MODULE");
+        return $res;
+    }
 
+    public function getAllGroupes(){
+        $res =  $this->queryAll("SELECT ANNEEGROUPE, INTITULEGROUPE FROM GROUPE");
+        return $res;
+    }
+
+    public function getAllEtudiants(){
+        $res = $this->queryAll("SELECT PRENOMETU,NOMETU FROM ETUDIANT");
+        return $res;
+    }
+
+    public function getAllProfesseurs(){
+        $res = $this->queryAll("SELECT PRENOMPROF, NOMEPROF FROM PROFESSEUR");
+        return $res;
+    }
 }
 ?>
