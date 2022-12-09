8a66d9ec686ecac7a6bd2537b94bbba84f1acb98
François Graux <93640820+SkyalphaBe@users.noreply.github.com>
2022-12-09 13:33:52 +0100

    création des méthodes pour insert admin

--- code/controllers/c_homeAdmin.php
+++ code/controllers/c_homeAdmin.php
@@ -3,7 +3,7 @@
 
     $dao = new AdminDAO(true, $_SESSION['login']);
 
-    /*$testP = $dao->createProf("bensligoat","1234","djamal","benslimane","PROFESSEUR");
-    $testE =$dao->createProf("Basti","1234","Bastien","Puscedo","ETUDIANT");*/
+    /*$testP = $dao->createCompte("bensligoat","1234","djamal","benslimane","PROFESSEUR");
+    $testE =$dao->createCompte("Basti","1234","Bastien","Puscedo","ETUDIANT");*/
     require_once(PATH_VIEWS.'AccueilAdmin.php');
 ?>
--- code/models/AdminDAO.php
+++ code/models/AdminDAO.php
@@ -2,7 +2,7 @@
 require_once (PATH_MODELS.'UserDAO.php');
 class AdminDAO extends UserDAO
 {
-    public function createProf($login,$password,$prenom,$nom,$typeCompte){
+    public function createCompte($login,$password,$prenom,$nom,$typeCompte){
         $mdp = password_hash($password, PASSWORD_DEFAULT);
         if($typeCompte == "PROFESSEUR"){
             $this->insertRow("insert into PROFESSEUR values (?,?,?,?)",[$login,$mdp,$prenom,$nom]);
@@ -10,6 +10,20 @@ class AdminDAO extends UserDAO
         else if($typeCompte == "ETUDIANT"){
             $this->insertRow("insert into ETUDIANT values (?,?,?,?)",[$login,$mdp,$prenom,$nom]);
         }
+    }
+
+    public function createGroupe($intituleGroupe,$anneGroupe){
+        $this->insertRow("insert into GROUPE values (?,?)",[$intituleGroupe,$anneGroupe]);
+    }
+    public function createModule($refModules,$nomModule,$descModules){
+        $this->insertRow("insert into MODULE values (?,?,?)",[$refModules,$nomModule,$descModules]);
+    }
+    public function assignerGroupe($intiuleGroupe,$anneGroupe,$refModule){
+        $this->insertRow("insert into PARTICIPER values(?,?,?)",[$intiuleGroupe,$anneGroupe,$refModule]);
+    }
+
+    public function assignerProf($loginProf,$refModule){
+        $this->insertRow("insert into ENSEIGNER values (?,?)",[$loginProf,$refModule]);
     }
 }
 ?>
