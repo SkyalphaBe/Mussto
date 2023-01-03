06395457f2e171011745d349d95276226f22d0da
François Graux <frsgra28@gmail.com>
2023-01-03 17:26:02 +0100

    correction de bug

--- code/api/creerCompteExcel.php
+++ code/api/creerCompteExcel.php
@@ -1,17 +1,15 @@
 <?php
-    header('Content-Type: text/plain; charset=utf-8');
+    header('Content-Type: application/json; charset=utf-8');
     require_once (PATH_MODELS.'AdminDAO.php');
     $data = json_decode(file_get_contents('php://input'), true);
     $response = [
-        'code'=> 200
+        'code'=> 400
     ];
-    if (!$data || json_last_error() === JSON_ERROR_NONE){
-        $response['code'] = 400;
-    }
-    if ($response['code']===200){
-        $dao = new AdminDAO(true, $_SESSION['login']);
-        $cpt = 0;
-        foreach ($data as $compte){
+    $dao = new AdminDAO(true, $_SESSION['login']);
+    $cpt = 0;
+    foreach ($data as $compte){
+        if (array_key_exists('login', $compte) && array_key_exists('mdp', $compte) && array_key_exists('prenom', $compte) && array_key_exists('nom', $compte) && array_key_exists('type', $compte)){
+            $response["code"] = 200;
             if ($compte["type"]==="ETUDIANT" || $compte["type"]==="PROFESSEUR"){
                 $dao->createCompte($compte["login"],$compte["mdp"],$compte["prenom"],$compte["nom"],$compte["type"]);
                 $cpt++;
