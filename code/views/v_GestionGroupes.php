9538ff0c3f0f5c5c6a15d6428373cf2c508f98b7
François Graux <frsgra28@gmail.com>
2023-01-09 11:57:59 +0100

    création ds groupes formulaire + debut création via excel

--- code/views/v_GestionGroupes.php
+++ code/views/v_GestionGroupes.php
@@ -17,8 +17,30 @@ require_once(PATH_VIEW_COMPONENT.'sideBarre.php');
         <label>Année 2<input type="radio" name="year" value="/api/listeGroupes/Annee2" ></label>
         <label>Année 3<input type="radio" name="year" value="/api/listeGroupes/Annee3" ></label>
     </div>
-    <button>Créer un Groupe</button>
+    <button id="btnCreer">Créer un Groupe</button>
 </div>
+<template>
+    <form method="post" class="formAdmin">
+        <div class="formContentAdmin">
+            <label>Intituler du groupe</label>
+            <input type="text" name="intitule" required>
+        </div>
+        <div class="formContentAdmin">
+            <label>Année du groupe</label>
+            <input type="text" name="annee" required>
+        </div>
+        <input type="submit" name="create" value="créer">
+    </form>
+</template>
+<template>
+    <div class="ExcelExport">
+        <a href="assets/excels/creerModule.xlsx" download>Télécharger excel exemple</a>
+        <form class="formAdminExcel">
+            <input type="file" id="fileGroup">
+            <input type="submit" id="sendItGroup">
+        </form>
+    </div>
+</template>
 <div class="content"></div>
 <script type="module" src="<?= PATH_SCRIPTS?>gestionAdminGroupes.js"></script>
 <?php
